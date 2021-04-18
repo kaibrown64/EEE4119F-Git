4f5303752200733e0236f1543c652c9fc0c4c6c2
Kai Brown <53183488+kaibrown64@users.noreply.github.com>
2021-04-18 20:41:40 +0200

    Q3 complete but not right
    Also corrected Q1

--- Prac_2.m
+++ Prac_2.m
@@ -3,30 +3,35 @@
 inputAmp = 50;
 inputBias = 40;
 delay = 0.5; %sec
+delayZ = 0.5/0.02; %Samples
 noiseSD = 0.5; %kPa
 Sens = 45.9/1000; %V/kPa
 Vs = 5; %Volts
 rangeMin = -15; %kPa
 rangeMax = 115; %kPa
-sampleTime = 0.2; %sec
+sampleTime = 0.02; %sec
 
 noisePow = noiseSD^2;
 s = tf('s');
 delayFn = exp(-delay*s);
+delayFn2 = tf(1, 1, sampleTime, 'InputDelay', delayZ);
 
-%% Question 4:
+%Question 3:
+T0 = 5; %deg celcius
+Cr = 100000; %J/K
+Qr1 = 1500; %W
+Qr2 = 2500; %W
+Qf = 1000; %W
+% Ql = Heat flow loss [W]
+Ta = 0; %°C
+K = 100; %W/°C
 
-function [Angles] = eulerMatrix(PQR)
-a = 1;
-b = 2;
-c = 3;
+Ton = 24;
+Toff = 26;
+delay2 = 3;
 
-stateSpace = [
-    1 sin(a)*cos(b) cos(a)*tan(b);
-    0 cos(a) -sin(a);
-    0 sin(a)*sec(b) cos(a)*sec(b)
-    ]
 
-Angles = PQR*stateSpace;
 
-end
+
+
+
